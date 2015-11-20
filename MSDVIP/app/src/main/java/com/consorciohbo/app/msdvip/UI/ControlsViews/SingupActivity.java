b4a23eafa72457153bcc15325f140dd85104795f
diego2608 <diegoif26@gmail.com>
2015-11-20 15:11:51 -0500

    Avance Perfil
    Corregir el botón validar se esta cruzando con la vista Crear cuenta.

--- MSDVIP/app/src/main/java/com/consorciohbo/app/msdvip/UI/ControlsViews/SingupActivity.java
+++ MSDVIP/app/src/main/java/com/consorciohbo/app/msdvip/UI/ControlsViews/SingupActivity.java
@@ -1,7 +1,10 @@
 package com.consorciohbo.app.msdvip.UI.ControlsViews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.consorciohbo.app.msdvip.R;
 
@@ -11,5 +14,19 @@ public class SingupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singup);
+
+        /*//-------------------------------------------------------------------------------------------
+        //Defino mi boton Guardar Formulario
+        Button btnSaveFormulario = (Button) findViewById(R.id.btnGuardarFormulario);
+
+        //Asigno el evenyo onclick al botn
+        btnSaveFormulario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent vistaFormulario = new Intent(SingupActivity.this, PerfilpageActivity.class);
+                startActivity(vistaFormulario);
+            }
+        });*/
     }
 }
