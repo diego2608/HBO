8c86a541b79a5a884ce8de4d9c7dfdb199f82191
diego2608 <diegoif26@gmail.com>
2015-12-07 18:03:54 -0500

    Popup

--- MSDVIP/app/src/main/java/com/consorciohbo/app/msdvip/EventosActivity.java
+++ MSDVIP/app/src/main/java/com/consorciohbo/app/msdvip/EventosActivity.java
@@ -1,14 +1,21 @@
 package com.consorciohbo.app.msdvip;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TabHost;
+import android.widget.Toast;
 
+import com.consorciohbo.app.library.MaterialDialog;
 
 public class EventosActivity extends AppCompatActivity {
+
+    MaterialDialog mMaterialDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +32,7 @@ public class EventosActivity extends AppCompatActivity {
         spec.setIndicator("Día");
         tabs.addTab(spec);
 
-        spec=tabs.newTabSpec("mitab2");
+        spec = tabs.newTabSpec("mitab2");
         spec.setContent(R.id.tab2);
         spec.setIndicator("Mes");
         tabs.addTab(spec);
@@ -39,6 +46,61 @@ public class EventosActivity extends AppCompatActivity {
             }
         });
 
+
     }
+
+
+    //-----------Popup
+
+    public void buttonPress(View view) {
+        // show imm
+        InputMethodManager imm = (InputMethodManager) this.getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,
+                InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    public void init(View v) {
+        mMaterialDialog = new MaterialDialog(this);
+    }
+
+    public void show(View v) {
+
+
+        if (mMaterialDialog != null) {
+            mMaterialDialog.setTitle("MaterialDialog").setMessage(
+                    "Hi! This is a MaterialDialog. It's very easy to use, you just new and show() it " +
+                            "then the beautiful AlertDialog will show automatically. It is artistic, conforms to Google Material Design." +
+                            " I hope that you will like it, and enjoy it. ^ ^")
+                    //mMaterialDialog.setBackgroundResource(R.drawable.background);
+                    .setPositiveButton("OK", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mMaterialDialog.dismiss();
+                            Toast.makeText(EventosActivity.this, "Agregado",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }).setNegativeButton("CANCEL", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mMaterialDialog.dismiss();
+                    Toast.makeText(EventosActivity.this, "Cancel", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }).setCanceledOnTouchOutside(false)
+                    // You can change the message anytime.
+                    .setOnDismissListener(
+                            new DialogInterface.OnDismissListener() {
+                                @Override
+                                public void onDismiss(DialogInterface dialog) {
+
+                                }
+                            }).show();
+        } /*else {
+            Toast.makeText(getApplicationContext(), "You should init firstly!",
+                    Toast.LENGTH_SHORT).show();
+        }*/
+    }
+
 
 }
